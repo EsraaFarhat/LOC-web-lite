6f49eb029291bcf8d4a1c9d286176631f513540b
EsraaFarhat <esraafarhat97@gmail.com>
2022-02-04 04:37:32 +0000

    Get Local Server Metrics

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const cors = require("cors");
 const express = require("express");
+const http = require("http");
+const os = require("os-utils");
+const checkDiskSpace = require("check-disk-space").default;
 require("dotenv").config();
 
 const authRouter = require("./routes/auth");
 const globalIdentifierRouter = require("./routes/globalIdentifier");
 const projectRouter = require("./routes/project");
 const LOCRouter = require("./routes/LOC");
+const auth = require("./middleware/auth");
 const error = require("./middleware/error");
 
 
@@ -16,8 +20,58 @@ require("./models/location")
 require("./models/LOC")
 require("./models/LOC_destination")
 
-
 const app = express();
+
+// ***************************************************************
+
+// let connections = 0;
+
+// const server = http.createServer(app);
+// server.listen(port);
+// server.on('connection', function (socket) {
+//    socket.on('data', function(data) {
+//       socket.write(data);
+//    });
+//    server.getConnections(function(err, count) {
+//      if(err) console.log(err);
+//       console.log("Connections: " + count);
+//       connections = count;
+//    });
+// }).on('close', function(socket){
+//   count--;
+//   connections = count;
+// });
+// app.get('/connections', (req, res) => {
+//   res.send({
+//       connections,
+//       // Total_Memory: os.totalmem() + ' MB', 
+
+      
+//   });
+// });
+// ***************************************************************
+
+
+let connected_users={};
+let Max_Connections;
+
+const server=http.createServer(app);
+
+server.on('connection',function(socket){ 
+  socket.setMaxListeners(100);
+  Max_Connections =  socket.getMaxListeners();
+  socket.__fd = socket.fd;
+  connected_users[socket.__fd]=socket.remoteAddress;
+  // console.log(connected_users);
+  // console.log(Object.keys(connected_users).length)
+  
+  socket.on('close',function(){
+      delete connected_users[socket.__fd];
+      // console.log(connected_users);
+  //     console.log(Object.keys(connected_users).length)
+  }); 
+});
+
 
 app.use(express.json());
 app.use(cors());
@@ -27,20 +81,40 @@ app.use("/api/globalIdentifiers", globalIdentifierRouter);
 app.use("/api/projects", projectRouter);
 app.use("/api/LOCs", LOCRouter);
 
+app.get("/api/localServerMetrics", auth, async (req, res) => {
+
+  const diskSpace = await checkDiskSpace("/");
+  const freeStorage = diskSpace.free / Math.pow(1000, 3);
+  const  totalStorage = diskSpace.size / Math.pow(1000, 3);
+  
+  os.cpuUsage(function(v){
+    return res.json({
+      Total_Storage: totalStorage + ' GB',
+      Free_Storage: freeStorage + ' GB',
+      CPU_Usage: v * 100 + ' %',
+      Total_Memory: os.totalmem() + ' MB', 
+      Free_Memory: os.freemem()  + ' MB',
+      Connections: Object.keys(connected_users).length,
+      Max_Connections
+    });
+  });
+  
+});
+
 app.use(error);
 
 process
-  .on("unhandledRejection", (reason, p) => {
-    console.error(reason, "Unhandled Rejection at Promise", p);
-    process.exit(1);
-  })
-  .on("uncaughtException", (err) => {
-    console.error(err.message, "Uncaught Exception thrown");
-    process.exit(1);
-  });
+.on("unhandledRejection", (reason, p) => {
+  console.error(reason, "Unhandled Rejection at Promise", p);
+  process.exit(1);
+})
+.on("uncaughtException", (err) => {
+  console.error(err, "Uncaught Exception thrown");
+  process.exit(1);
+});
 
 if (!process.env.PRIVATE_KEY) {
   throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
 }
 
-module.exports = app;
+module.exports = server;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const app = require("./app");
+const server = require("./app");
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+server.listen(port, "10.42.0.253", null, () => { 
   console.log(`Server is listening at http://localhost:${port}`);
 });
