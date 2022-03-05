b63b7c006a60379e8acb2c602ace2beb79cd2a1c
EsraaFarhat <esraafarhat97@gmail.com>
2022-03-05 09:59:00 +0200

    Exit after creating tables

--- db/postgres/tables.js
+++ db/postgres/tables.js
@@ -1,20 +1,19 @@
 const sequelize = require("./db");
 
-
 (async function createTables() {
-try {
-await sequelize.sync({ force: true });
-// await sequelize.model("User").create({
-//   fullName: "admin",
-//   email: "admin@gmail.com",
-//   password: "admin12345",
-//   role: "admin",
-// });
+  try {
+    await sequelize.sync({ force: true });
+    // await sequelize.model("User").create({
+    //   fullName: "admin",
+    //   email: "admin@gmail.com",
+    //   password: "admin12345",
+    //   role: "admin",
+    // });
 
-console.log("Tables created successfully...")
-  } catch (error) {
+    console.log("Tables created successfully...");
+    process.exit();
+} catch (error) {
     console.log(error);
+    process.exit(1);
   }
 })();
-
-process.exit()
