d86cb1b44e89cb31611f553077601cff583a0110
EsraaFarhat <esraafarhat97@gmail.com>
2022-09-16 12:34:49 +0200

    deploying to heroku

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// const app = require("./app");
-const express = require("express");
+const winston = require('winston');
+const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3001;
-
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    winston.info(`listening on port ${port}...`);
 });
